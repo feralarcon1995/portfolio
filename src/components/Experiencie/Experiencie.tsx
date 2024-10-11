1be134dcdf2697b867c7c2e3ce528c00f25088bb
Fernando Alarcon <feralarcon1995@gmail.com>
2024-10-11 13:39:15 -0300

    fix slider

--- src/components/Experiencie/Experiencie.tsx
+++ src/components/Experiencie/Experiencie.tsx
@@ -3,7 +3,7 @@ import Lenis from '@studio-freight/lenis';
 import { motion, useAnimation } from 'framer-motion';
 import Image from 'next/image';
 import styles from './style.module.scss';
-import { RButton } from '../Button/RButton';
+// import { RButton } from '../Button/RButton';
 import { ArrowUp } from '@/icons/ArrowUp';
 export default function Experiencie() {
   const experiences = [
@@ -58,18 +58,18 @@ export default function Experiencie() {
 
   const stackColors: Record<string, { bgColor: string; textColor: string, hoverColor: string }> = {
     nextjs: { bgColor: 'var(--light)', textColor: 'var(--light)', hoverColor: 'var(--black)' },
-    typescript: { bgColor: '#009cff', textColor: '#009cff', hoverColor: '#000' },
-    aws: { bgColor: '#ff6a00', textColor: '#ff6a00', hoverColor: '#000' },
+    typescript: { bgColor: '#009cff', textColor: '#009cff', hoverColor: 'var(--light)' },
+    aws: { bgColor: '#ff6a00', textColor: '#ff6a00', hoverColor: 'var(--light)' },
     nodejs: { bgColor: '#167614', textColor: '#167614', hoverColor: 'var(--light)' },
-    materialui: { bgColor: '#0070f3', textColor: '#0070f3', hoverColor: '#000' },
-    html: { bgColor: '#e34c26', textColor: '#e34c26', hoverColor: '#000' },
-    css: { bgColor: '#264de4', textColor: '#264de4', hoverColor: '#000' },
-    javascript: { bgColor: '#f7df1e', textColor: '#f7df1e', hoverColor: '#000' },
-    bootstrap: { bgColor: '#B175FF', textColor: '#B175FF', hoverColor: 'var(--black)' },
-    git: { bgColor: '#f05032', textColor: '#f05032', hoverColor: '#000' },
-    scss: { bgColor: '#c6538c', textColor: '#c6538c', hoverColor: '#000' },
-    python: { bgColor: '#306998', textColor: '#306998', hoverColor: '#000' },
-    django: { bgColor: '#88CA5E', textColor: '#88CA5E', hoverColor: '#000' },
+    materialui: { bgColor: '#0070f3', textColor: '#0070f3', hoverColor: 'var(--light)' },
+    html: { bgColor: '#e34c26', textColor: '#e34c26', hoverColor: 'var(--light)' },
+    css: { bgColor: '#264de4', textColor: '#264de4', hoverColor: 'var(--light)' },
+    javascript: { bgColor: '#f7df1e', textColor: '#f7df1e', hoverColor: 'var(--light)' },
+    bootstrap: { bgColor: '#B175FF', textColor: '#B175FF', hoverColor: 'var(--light)' },
+    git: { bgColor: '#f05032', textColor: '#f05032', hoverColor: 'var(--light)' },
+    scss: { bgColor: '#c6538c', textColor: '#c6538c', hoverColor: 'var(--light)' },
+    python: { bgColor: '#306998', textColor: '#306998', hoverColor: 'var(--light)' },
+    django: { bgColor: '#88CA5E', textColor: '#88CA5E', hoverColor: 'var(--light)' },
     reactjs: { bgColor: '#61dafb', textColor: '#61dafb', hoverColor: '#000' },
     default: { bgColor: '#ccc', textColor: '#000', hoverColor: '#000' },
   };
@@ -137,42 +137,77 @@ export default function Experiencie() {
           <motion.h3 animate={subTitleControls}>My Journey: What I&apos;ve Learned in the Way</motion.h3>
         </motion.article>
 
-        {experiences.map((exp, index) => (
-          <article className={styles.experience_details} key={index}>
-            <div className={styles.experience_header}>
-              <p className={styles.experience_date}>{exp.date}</p>
-              <h3>{exp.title}</h3>
-              <a href={exp.link_company} target="_blank" rel="noopener noreferrer">
-                <Image
-                  src={exp.thumbnail}
-                  alt={`${exp.company} logo`}
-                  width={40}
-                  height={40}
-                  className={styles.experience_thumbnail}
-                />   {exp.company} <ArrowUp />
-              </a>
-            </div>
-            <p className={styles.experience_description}>{exp.description}</p>
-            <div className={styles.experience_stack_container}>
-              <h4>stack in which I work</h4>
-              <div>
-                {exp.stack.map((item) => {
-                  const { bgColor, textColor, hoverColor } = stackColors[item.id] || stackColors.default;
-                  return (
-                    <RButton
-                      key={item.id}
-                      text={item.name}
-                      color={bgColor}
-                      color_text={textColor}
-                      hover_color={hoverColor}
-                      font_size='1.2rem'
-                    />
-                  );
-                })}
+        {experiences.map((exp, index) => {
+          const baseDuration = 10;
+          const calculatedDuration = baseDuration + exp.stack.length * 1.2;
+          return (
+            <article className={styles.experience_details} key={index}>
+              <div className={styles.experience_header}>
+                <p className={styles.experience_date}>{exp.date}</p>
+                <h3>{exp.title}</h3>
+                <a href={exp.link_company} target="_blank" rel="noopener noreferrer">
+                  <Image
+                    src={exp.thumbnail}
+                    alt={`${exp.company} logo`}
+                    width={40}
+                    height={40}
+                    className={styles.experience_thumbnail}
+                  />   {exp.company} <ArrowUp />
+                </a>
               </div>
-            </div>
-          </article>
-        ))}
+              <p className={styles.experience_description}>{exp.description}</p>
+              <div className={styles.experience_stack_container}>
+                <h4>Stack in which I work</h4>
+                <div
+                  className={styles.slider}
+                  style={{
+                    '--width': '270px',
+                    '--quantity': `${exp.stack.length}`,
+                    '--height': '50px',
+                    '--duration': `${calculatedDuration}s`
+                  } as React.CSSProperties}>
+                  <div className={styles.slider_list}>
+                    {exp.stack.map((item, index) => {
+                      const { bgColor, hoverColor } = stackColors[item.id] || stackColors.default;
+                      return (
+                        <div key={item.id} className={styles.slider_item} style={{ '--position': `${index + 1}` } as React.CSSProperties}>
+                          <span style={{
+                            '--hover': `${bgColor}`,
+                            '--text-hover': `${hoverColor}`
+                          } as React.CSSProperties}>{item.name}</span>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+                <div
+                  className={`${styles.slider} ${styles.reverse}`}
+                  style={{
+                    '--width': '270px',
+                    '--quantity': `${exp.stack.length}`,
+                    '--height': '50px',
+                    '--duration': `${calculatedDuration +5}s`
+                  } as React.CSSProperties}>
+                  <div className={styles.slider_list}>
+                    {exp.stack.map((item, index) => {
+                      const { bgColor, hoverColor } = stackColors[item.id] || stackColors.default;
+                      return (
+                        <div key={item.id} className={styles.slider_item} style={{ '--position': `${index + 1}` } as React.CSSProperties}>
+                          <span style={{
+                            '--hover': `${bgColor}`,
+                            '--text-hover': `${hoverColor}`
+                          } as React.CSSProperties}>{item.name}</span>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+            </article>
+          );
+        })}
+
+
       </article>
     </section>
   );
